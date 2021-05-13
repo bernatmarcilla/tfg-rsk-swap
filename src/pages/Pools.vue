b3b6bd76484b6e016dbe1ac70f13e31353de4ced
ikcoin <ikcoin.kb@protonmail.com>
2021-05-13 23:32:21 +0200

    AddLiquidityETH functionality

--- src/pages/Pools.vue
+++ src/pages/Pools.vue
@@ -36,7 +36,7 @@
           <br>
           <div id="buttons" style="display: table;margin: 0 auto;">
             <base-button @click="approveContract('A')" type="primary" :disabled="approvePoolA" fill> 1. Approve Contract </base-button>
-            <base-button @click="swapTokens()" type="primary" :disabled="!approvePoolA" fill> 2. Add Liquidity </base-button>
+            <base-button @click="addLiquidity('A')" type="primary" :disabled="!approvePoolA" fill> 2. Add Liquidity </base-button>
           </div>
 
     </card>
@@ -73,12 +73,12 @@
           <br>
 
           <div>
-            <h5><i style="color:#2dce89" v-if="this.conversionValueB" class="tim-icons icon-alert-circle-exc"></i> <span class="text-success"> <b v-if="this.conversionValueB">Actual Price: </b> {{this.conversionValueB}}{{this.conversionTokensB}}</span></h5>
+            <h5><i style="color:#2dce89" v-if="this.conversionValueB" class="tim-icons icon-alert-circle-exc"></i> <span class="text-success"> <b v-if="this.conversionValueB" class="text-success">Actual Price: </b> {{this.conversionValueB}}{{this.conversionTokensB}}</span></h5>
           </div>
           <br>
         <div id="buttons" style="display: table;margin: 0 auto;">
           <base-button @click="approveContract('B')" type="primary" :disabled="approvePoolB" fill> 1. Approve Contract </base-button>
-          <base-button @click="swapTokens()" type="primary" :disabled="!approvePoolB" fill> 2. Add Liquidity </base-button>
+          <base-button @click="addLiquidity('B')" type="primary" :disabled="!approvePoolB" fill> 2. Add Liquidity </base-button>
         </div>
     </card>
     </div>
@@ -137,8 +137,8 @@
     methods: {
       calculateConversion: function(pool){
         console.log('calculateConversionFunction');
-          var TOKEN2_ADDRESS='';
-          let stringValue='';
+          var TOKEN2_ADDRESS;
+          let stringValue;
           if (pool == this.tokens[0]) //tokenA
           {
             TOKEN2_ADDRESS = contractsInfo.tokenAContract.address;
@@ -164,57 +164,61 @@
             }
           })
       },
-     addLiquidity: function(){
+     addLiquidity: function(pool){
+        var inputToken;
+        var inputValue;
+        var outputValue;
 
-        if (window.ethereum) {
-            
-            web3.eth.net.getId().then(x=>{
-                if (x != this.routerContract.chainId){
-                    alert('Change metamask wallet chain and repeat');
+        var that = this;
+        web3.eth.net.getId().then(x=>{
+            if (x != that.routerContract.chainId){
+                alert('Change metamask wallet chain and repeat');
+            } else {
+                if (pool == 'A'){
+                  inputToken = contractsInfo.tokenAContract.address;
+                  inputValue = String(that.inputValueA.toString()+'000000000000000000');
+                  outputValue = String(that.outputValueA * (10**18));
                 } else {
-                    this.routerContract.address = '0xf55c496bb1058690DB1401c4b9C19F3f44374961';
-
-
-                    var that = this;
-                    var defaultAccount;
-                        window.ethereum.enable().then(function(){
-                            web3.eth.getAccounts(function(err, result){
-                                if (err){
-                                    alert(err);
-                                } else {
-                                    that.defaultAccount = result[0];
-                                    var contractTokenA = new web3.eth.Contract(that.tokenA.abi, that.tokenA.address.toString().toLowerCase());
-                                    var contractRouter = new web3.eth.Contract(that.routerContract.abi, that.routerContract.address);
-                    
-                                    //approve(spender, amount) 100000000000000000000 == 100 tokensA
-                                    //contractTokenA.methods.approve(that.routerContract.address, 100000000000000000000).call().then(function(result){
-                                    //if (result){ //  token, amountTokenDesired, amountTokenMi, amountEthMin, to, deadline
-                                        contractRouter.methods.addLiquidityETH(that.tokenA.address, '100000000000000000000', '100000000000000000000', '1000000000000000', defaultAccount, '1620395772').send({
-                                            from: defaultAccount,
-                                            value: 1000000000000000
-                                        });
-                                    //}
-                                    //});
-                                }
-                            })
-                        })
-                    
-
-                    var contract = new web3.eth.Contract(this.routerContract.abi, this.routerContract.address);
-                    //contract.methods.addLiquidityETH(this.tokenA,)
+                  inputToken = contractsInfo.tokenBContract.address;
+                  inputValue = String(that.inputValueB.toString()+'000000000000000000');
+                  outputValue = String(that.outputValueB * (10**18));
                 }
-            })
-        } else {
-        alert('No web3? You should consider trying MetaMask!');
-        }
+                var that2 = that;
+                var defaultAccount;
+                    window.ethereum.enable().then(function(){
+                        web3.eth.getAccounts(function(err, result){
+                            if (err){
+                                alert(err);
+                            } else {
+                                var contractRouter = new web3.eth.Contract(contractsInfo.routerContract.abi, contractsInfo.routerContract.address.toString().toLowerCase());
+
+                                console.log(inputToken, inputValue, outputValue, that2.defaultAccount);
+                                contractRouter.methods.addLiquidityETH(inputToken, inputValue, inputValue, outputValue, that2.defaultAccount, '0xFFFFFFFFFF').send({
+                                    from: that2.defaultAccount,
+                                    value: outputValue
+                                }); //gasLimit = 3000000
+                            }
+                        })
+                    })
+                
+
+                var contract = new web3.eth.Contract(this.routerContract.abi, this.routerContract.address);
+                //contract.methods.addLiquidityETH(this.tokenA,)
+            }
+        })
     },
     approveContract: function(pool){
       
+      var stringValue ='';
+
       if (pool == 'A'){
-        var tokenContract = new web3.eth.Contract(this.tokenA.abi, this.tokenA.address.toString().toLowerCase());
+        var tokenContract = new web3.eth.Contract(contractsInfo.tokenAContract.abi, contractsInfo.tokenAContract.address.toString().toLowerCase());
         var approveButton = function(){that.approvePoolA = true};
+        stringValue = String(this.inputValueA.toString()+'000000000000000000'); 
       } else {
-        //var tokenContract = new web3.eth.Contract(this.tokenB.abi, this.tokenA.address.toString().toLowerCase());
+        var tokenContract = new web3.eth.Contract(contractsInfo.tokenBContract.abi, contractsInfo.tokenBContract.address.toString().toLowerCase());
+        var approveButton = function(){that.approvePoolB = true};
+        stringValue = String(this.inputValueB.toString()+'000000000000000000'); 
       }
       
       var that = this;
@@ -224,7 +228,7 @@
           if (!err){
               that.defaultAccount = result[0];
               //alert(that.routerContract.address);
-              tokenContract.methods.approve('0xf55c496bb1058690DB1401c4b9C19F3f44374961', '100000000000000000000').send({
+              tokenContract.methods.approve(contractsInfo.routerContract.address.toString().toLowerCase(), stringValue).send({
                 from: that.defaultAccount
             });
             approveButton();
