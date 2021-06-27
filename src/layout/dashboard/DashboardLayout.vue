a884be41e42702e92000eaa48f2ce5d55d57de72
ikcoin <ikcoin.kb@protonmail.com>
2021-06-27 21:44:16 +0200

    json format

--- src/layout/dashboard/DashboardLayout.vue
+++ src/layout/dashboard/DashboardLayout.vue
@@ -4,8 +4,6 @@
       <template slot="links">
         <sidebar-link to="/swap" :name="$t('sidebar.swap')" icon="tim-icons icon-double-right"/>
         <sidebar-link to="/pools" :name="$t('sidebar.pools')" icon="tim-icons icon-app"/>
-        <sidebar-link to="/typography" :name="$t('sidebar.typography')" icon="tim-icons icon-align-center"/>
-        <sidebar-link to="/icons" :name="$t('sidebar.icons')" icon="tim-icons icon-atom"/>
       </template>
     </side-bar>
     <div class="main-panel">
